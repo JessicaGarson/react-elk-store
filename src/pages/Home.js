ec970b3248f9330404ec74fc683646ea3c80a769
Jessica Garson <jess.garson@elastic.co>
2025-02-17 14:54:08 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -68,7 +68,7 @@ const Home = () => {
       </div>
 
       <div className="container mx-auto max-w-7xl px-6 py-10">
-        <h1 className="text-4xl font-bold text-gray-800 mb-8 text-right">Elk Store</h1>
+        <h1 className="text-4xl font-bold text-gray-800 mb-8 text-left">Elk Store</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md-grid-cols-3 lg:grid-cols-4 gap-8">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} onAddToCart={handleAddToCart} />
